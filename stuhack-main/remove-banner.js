be9b87494966328fa55bdded8638f25ba567c033
Sơn Nguyễn <85350863+sonnguyen1004@users.noreply.github.com>
2023-09-12 21:56:14 +0700

    Add files via upload

---
+++ stuhack-main/remove-banner.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function(){
+	var banner = document.getElementById('document-wrapper')
+	if(banner != null){
+		var banners = banner.childNodes;
+		if (banners.length>3){
+			banners[0].parentNode.removeChild(banners[0]);
+		}
+	}	
+	var premiumButton = document.getElementById('header-position-handle').childNodes[0].childNodes[1].childNodes[0].childNodes[1];
+	premiumButton.parentNode.removeChild(premiumButton);
+
+	/* Mobile */
+	if (window.innerWidth <= 990){
+		var container = document.getElementById('page-container');
+		if(container != null){
+			var pages = container.childNodes;
+			for(i=0; i<pages.length; i++) {
+				if(pages[i].id == ''){
+					pages[i].parentNode.removeChild(pages[i]);
+				}
+			}
+		}
+	}
+
+    try{
+		var recomendations = document.getElementById('viewer-recommendations');
+		if(recomendations != null){
+			recomendations.parentNode.parentNode.removeChild(recomendations.parentNode);
+		}
+	}catch(err){
+		console.log(err);
+	}
+	
+});
